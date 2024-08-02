3843e6855991f99d7df9cb922a3cbaa9de868fa1
TheDX2121 <129209401+TheDX2121@users.noreply.github.com>
2024-08-03 00:51:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,7 +48,7 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://TeamDx:TeamDx@cluster0.lvc5j7k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Sparkle_Autobot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'SparkleCollection')
 
