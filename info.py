146ac41b0874ec5ed190952e5d707c46471e48fa
TheDX2121 <129209401+TheDX2121@users.noreply.github.com>
2024-09-23 12:24:11 +0530

    Update info.py

--- info.py
+++ info.py
@@ -103,9 +103,9 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://t.me/How_To_Open_Linkl')
 
 # Shortlink Info
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'api.shareus.io')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'hRPS5vvZc0OGOEUQJMJzPiojoVK2')
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True))
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'modijiurl.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', 'c66c2a37662a3dccddb7123be43691ec27e7ef4e')
 
 # Others
 MAX_B_TN = environ.get("MAX_B_TN", "5")
